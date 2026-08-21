814fe7446c2cd53cc9135ae9011914bde2267ba8
Shubham Joshi <45368598+shubham-1123@users.noreply.github.com>
2026-08-21 20:37:27 +0530

    Solve sigmoid-numpy

---
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def sigmoid(x: list | float) -> np.ndarray | float:
+    """
+    Vectorized sigmoid function.
+    """
+    # Write code here
+    x = np.asarray(x)
+    result = np.empty_like(x, dtype = float)
+    positive = x >= 0
+    result[positive] = 1/(1 + np.exp(-x[positive]))
+
+    exp_x = np.exp(x[~positive])
+    result[~positive] = exp_x / (1+exp_x)
+
+    return result
