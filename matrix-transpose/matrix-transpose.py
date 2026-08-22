2b7462a7965dbbd8a998e1acba992e72507d62a0
Shubham Joshi <45368598+shubham-1123@users.noreply.github.com>
2026-08-22 10:31:45 +0530

    Solve matrix-transpose

--- matrix-transpose/matrix-transpose.py
+++ matrix-transpose/matrix-transpose.py
@@ -6,5 +6,15 @@ def matrix_transpose(A: list) -> np.ndarray:
     """
     # Write code here
     x = np.asarray(A)
-    return x.T
+    rows, cols = x.shape
+    y = np.empty(shape=(x.shape[1], x.shape[0]), dtype = float)
+
+    for i in range(rows):
+        for j in range(cols):
+            y[j][i] = x[i][j]
+    
+    return y
+
+
+    
     
